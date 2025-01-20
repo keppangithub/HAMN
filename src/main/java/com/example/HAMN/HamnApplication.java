da7bff957098c9a408dddfab1b057240b4017904
keppangithub <kevinnordkvist@hotmail.com>
2025-01-20 17:28:40 +0100

    Project Setup
    Initialize spring-boot-application

---
+++ src/main/java/com/example/HAMN/HamnApplication.java
@@ -0,0 +1,12 @@
+package com.example.HAMN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/HAMN/HamnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HAMN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
