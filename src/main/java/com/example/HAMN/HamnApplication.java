a36f197ccc049e6298dbf0b73d45ede9d80407f6
keppangithub <kevinnordkvist@hotmail.com>
2025-01-20 18:48:03 +0100

    Gradle and DB setup

--- src/main/java/com/example/HAMN/HamnApplication.java
+++ src/main/java/com/example/HAMN/HamnApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HamnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HamnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HamnApplication.class, args);
+    }
 }
